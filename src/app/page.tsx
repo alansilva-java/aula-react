a2833f2415693920c6c8e20c209aee83d5a5d2c5
Alan.S <alan.silva-17@hotmail.com>
2024-09-03 22:42:55 -0300

    list-reduc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,90 @@
 "use client"
 
-import { useState } from "react";
-import { Item } from "./types/Item";
+import { listReducer } from "@/reducers/listReducer";
+import { useReducer, useState } from "react";
 
 
 const Page = () => {
-  const [list, setList] = useState<Item[]>([]);
+  const [list, dispatch] = useReducer(listReducer, []);
+  const [addField, setAddField] = useState ('');
+  const  handleAddButton = () => {
+    if(addField.trim() === '') return false;
+    dispatch({
+      type: 'add',
+      payload: {
+        text: addField.trim()
+      }
+    })
 
-  const addNewItem =(text: string) => {
-    setList([...list, {
-      id: list.length,
-      text,
-      done: false
-
-    }]);
+    setAddField('');
   }
 
-  const editItemText = (id:number,newText:string) => {
-    setList(
-      list.map (t=> {
-        if (t.id === id) t.text = newText;
-        return t;
-      })
-    );
+  const handleDoneCheckBox = (id: number) => {
+     dispatch({
+      type:'toggleDone',
+      payload : { id }     
+     });
   }
 
-  const toggleItem = (id:number) =>{
-    setList(
-      list.map(t => {
-        if (t.id === id) t.done = !t.done;
-        return t
-      })
-    );
+  const handleEdit = (id:number) => {
+    const item = list.find(it => it.id === id);
+    if (!item) return false;
+
+    const newText = window.prompt('Editar Tarefa', item.text)
+    if (!newText || newText.trim() === '') return false;
+
+    dispatch({
+      type: 'editText',
+      payload: {id,newText}
+    }); 
   }
 
-  const removeItem = (id: number) => {
-    setList (list.filter(t => t.id !== id ) );
+  const handleRemove = (id:number) => {
+    if(!window.confirm('Tem certeza que deseja excluir')) return false;
+    dispatch({
+      type:'remove',
+      payload:{id}
+    })
   }
-
   return (
-    <div className="">
-     
+    <div className="continer mx-auto">
+      <h1 className="text-center text-4xl my-4">Lista de Tarefas</h1>
+      <div className=" max-w-2xl bg-gray-100 mx-auto flex rounded-md border border-gray-400 p-4 my-4">
+        <input 
+        type="text" 
+        className=" flex-1 rounded-md border border-black p-3 bg-transparent text-black outline-none "
+        placeholder="Digite um item" 
+        value={addField}
+        onChange={e => setAddField(e.target.value)}
+        />
+        <button 
+        className="p-4"
+        onClick={handleAddButton}
+        >Adcionar</button>
+      </div>
+      <ul className="max-w-2xl mx-auto">
+        {
+          list.map(item => (
+            <li 
+              key = {item.id}
+              className="flex items-center p-3 my-3 border-b border-gray-700 "
+              >
+                <input type="checkbox" 
+                className="w-6 h-6 mr-4"
+                defaultChecked={item.done}
+                onClick={() => handleDoneCheckBox(item.id)}
+                />
+              <p className="flex-1 text-lg">{item.text}</p>
+              <button onClick={() => handleEdit(item.id)} className="mx-4 text-black hover:text-gray-400">Editar</button>
+              <button onClick={() => handleRemove(item.id)} className="mx-4  text-black hover:text-gray-400">Excluir</button>
+              </li>
+          ))
+        }
+      </ul>
+      
     </div>
-  );
+  )
+
 }
 
 export default Page;
--- src/reducers/listReducer.ts
+++ src/reducers/listReducer.ts
@@ -22,8 +22,10 @@ type ToggleDoneAction = {
 }
 
 type RemoveAction = {
-    type: 'remove'
-    payload : number;
+    type: 'remove';
+    payload :{
+        id: number;
+    }
 }
 
 
@@ -32,7 +34,32 @@ type RemoveAction = {
 type ListActions = AddAction | EditTextAction | ToggleDoneAction | RemoveAction;
 
 export const listReducer = (list: Item[], action:ListActions ) => {
-    
+    switch(action.type) {
+        case 'add':
+            return [...list, {
+                id: list.length,
+                text: action.payload.text,
+                done: false
+            }]
+        case 'editText':
+            return list.map( t => {
+                if (t.id === action.payload.id) {
+                     t.text = action.payload.newText;
+                }
+                return t;
+            });
+        case 'toggleDone':
+            return list.map (t=> {
+                if (t.id === action.payload.id) t.done = !t.done;
+                return t ;
+            })
+
+        case 'remove':
+            return list.filter (t => t.id !== action.payload.id);
+            default:
+                return list;
+
+    }
 
     return list ;
 }
